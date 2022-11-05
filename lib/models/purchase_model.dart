09479d76a8e54b1e905e6b7701e98f5c9ecfe480
muradhossin <murad15-2547@diu.edu.bd>
2022-11-05 18:42:09 +0600

    purchase model

--- lib/models/purchase_model.dart
+++ lib/models/purchase_model.dart
@@ -1,8 +1,20 @@
+
+
 import 'package:ecommerce_admin/models/date_model.dart';
 
-class PurchaseModel {
+const String collectionPurchase='Purchases';
+
+const String purchaseFieldId='purchaseId';
+const String purchaseFieldProductId='productId';
+const String purchaseFieldQuantity='purchaseQuantity';
+const String purchaseFieldPrice='purchasePrice';
+const String purchaseFieldDateModel='dateModel';
+
+
+class PurchaseModel{
+
   String? purchaseId;
-  String? productId;
+  String ? productId;
   num purchaseQuantity;
   num purchasePrice;
   DateModel dateModel;
@@ -14,4 +26,22 @@ class PurchaseModel {
     required this.purchasePrice,
     required this.dateModel,
   });
+
+  Map<String,dynamic>toMap(){
+    return <String,dynamic>{
+      purchaseFieldId:purchaseId,
+      purchaseFieldProductId:productId,
+      purchaseFieldQuantity:purchaseQuantity,
+      purchaseFieldPrice:purchasePrice,
+      purchaseFieldDateModel:dateModel.toMap(),
+    };
+  }
+
+  factory PurchaseModel.fromMap(Map<String,dynamic>map)=>PurchaseModel(
+    purchaseId:map[purchaseFieldId],
+    productId:map[purchaseFieldProductId],
+    purchaseQuantity: map[purchaseFieldQuantity],
+    purchasePrice:map[purchaseFieldPrice],
+    dateModel: DateModel.fromMap(map[purchaseFieldDateModel]),
+  );
 }
