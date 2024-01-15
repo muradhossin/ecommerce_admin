82af4954a808f586c5e169bc6a773e30844164b8
muradhossin <murad15-2547@diu.edu.bd>
2024-01-15 23:48:35 +0600

    image path extension added

---
+++ lib/core/extensions/image_path.dart
@@ -0,0 +1,17 @@
+extension ImagePath on String {
+  String get pngImage => 'assets/images/$this.png';
+  String get jpgImage => 'assets/images/$this.jpg';
+  String get svgImage => 'assets/images/$this.svg';
+
+  String get pngIcon => 'assets/icons/$this.png';
+  String get jpgIcon => 'assets/icons/$this.jpg';
+  String get svgIcon => 'assets/icons/$this.svg';
+
+  String get gifImage => 'assets/gif/$this.gif';
+
+}
+
+
+class Images {
+  static String logo = 'logo'.pngIcon;
+}
